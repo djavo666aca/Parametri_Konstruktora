20c2a10b9273f42c9e23ed4d50c65383d7e19e30
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-08 19:49:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Parametri_Konstruktora
+{
+    internal class Program
+    {
+        string Radnja;
+        int Telefon;
+        char Slovo_grada;
+
+        Program(string R, int T, char S) 
+        {
+        Radnja = R;
+            Telefon = T;
+               Slovo_grada = S;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p1 = new Program("Maxi", 069,'B') ;
+            
+            Console.WriteLine(p1.Radnja);
+            Console.WriteLine(p1.Telefon);
+            Console.WriteLine(p1.Slovo_grada);
+
+            Program p2 = new Program("Idea", 061, 'N');
+
+            Console.WriteLine(p2.Radnja);
+            Console.WriteLine(p2.Telefon);
+            Console.WriteLine(p2.Slovo_grada);
+        }
+    }
+}
